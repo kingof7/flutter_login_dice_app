b6cebab6aa6f0ba12e8ffcb0d6a8e46d9fb3d53b
vlfl0 <vlfl0@naver.com>
2023-02-26 23:20:35 +0900

    로그인 part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dice.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,9 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,79 +35,142 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(
-                top: 50,
-              ),
-            ),
-            Center(
-              child: Image(
-                  image: AssetImage('image/chef.gif'),
-                  width: 170.0,
-                  height: 190.0),
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15.0,
-                    ),
+      body: Builder(
+        builder: (context) {
+          return SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 50,
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter dice',
+                Center(
+                  child: Image(
+                      image: AssetImage('image/chef.gif'),
+                      width: 170.0,
+                      height: 190.0),
+                ),
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15.0,
                         ),
-                        keyboardType: TextInputType.emailAddress,
                       ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter password',
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          SizedBox(height: 40.0),
-                          ButtonTheme(
-                            minWidth: 100.0,
-                            height: 50.0,
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                textStyle: MaterialStateProperty.all(
-                                  TextStyle(fontSize: 14, color: Colors.white),
-                                ),
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.orangeAccent),
-                              ),
-                              child: Icon(
-                                Icons.arrow_forward,
-                              ),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                              labelText: 'Enter dice',
                             ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            controller: controller2,
+                            decoration: InputDecoration(
+                              labelText: 'Enter password',
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(height: 40.0),
+                              ButtonTheme(
+                                minWidth: 100.0,
+                                height: 50.0,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (controller.text == 'dice' &&
+                                        controller2.text == '1234') {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (BuildContext context) =>
+                                                  Dice()));
+                                    } else if (controller.text == 'dice' &&
+                                        controller2.text != '1234') {
+                                      showSnackBar2(context);
+                                    } else if (controller.text != 'dice' &&
+                                        controller2.text == '1234') {
+                                      showSnackBar3(context);
+                                    } else {
+                                      showSnackBar(context);
+                                    }
+                                  },
+                                  style: ButtonStyle(
+                                    textStyle: MaterialStateProperty.all(
+                                      TextStyle(
+                                          fontSize: 14, color: Colors.white),
+                                    ),
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Colors.orangeAccent),
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        '로그인 정보를 다시 확인하세요.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
+}
+
+void showSnackBar2(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        '비밀번호가 일치하지 않습니다.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
+}
+
+void showSnackBar3(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        'dice의 철지를 확인하세요.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    ),
+  );
 }
