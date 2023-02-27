dda96afd13b67874cb296cb1313e21b79d0ec0c3
kingof7 <vlfl0@naver.com>
2023-02-27 22:58:53 +0900

    part3 : dice적용, 가운데정렬

--- lib/dice.dart
+++ lib/dice.dart
@@ -11,10 +11,24 @@ class Dice extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image(image: Asse,)
+                Image(
+                  image: AssetImage('image/dice1.png'),
+                  width: 150.0,
+                  height: 150.0,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image(
+                  image: AssetImage('image/dice2.png'),
+                  width: 150.0,
+                  height: 150.0,
+                ),
               ],
             ),
           ],
