875b76672fe70622098deb78cd8d824b405931e4
kingof7 <vlfl0@naver.com>
2023-03-01 00:45:57 +0900

    강좌 5| 로그인과 주사위 게임 플러터 앱 만들기 part 3

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,23 +22,46 @@ class Dice extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image(
-                  image: AssetImage('image/dice1.png'),
-                  width: 150.0,
-                  height: 150.0,
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Image.asset('image/dice$leftDice.png'),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Image.asset('image/dice$rightDice.png'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 60.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                    rightDice = Random().nextInt(6) + 1;
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.orangeAccent,
+                  ),
                 ),
-                SizedBox(
-                  width: 20,
+                child: Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 50.0,
                 ),
-                Image(
-                  image: AssetImage('image/dice2.png'),
-                  width: 150.0,
-                  height: 150.0,
-                ),
-              ],
+              ),
             ),
           ],
         ),
